9d0900381e3bcd91a66b49c9dd04b2410d815b20
Weihan Li <lwh990316@gmail.com>
2017-11-12 12:00:53 -0500

    Create final-demo.py

---
+++ final-demo.py
@@ -0,0 +1,7 @@
+# Team name: None
+# Team members: Lily Chen, huipingc
+# Project: Real Time Face Filter
+#               Anita Li, weihanl1
+#               Hang Yin, hyin2
+#               Peizhi Yu, peizhiy
+# https://drive.google.com/a/andrew.cmu.edu/file/d/1bBVPnMVQ6C8RONV1uKLrkncaQfdO52IB/view?usp=sharing
